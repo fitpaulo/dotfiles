69df9778a926e1c1f15f358570f9c7de8c001e3e
Paulo <paulotechusa@protonmail.com>
2023-12-11 19:24:55 -0600

    daily updates

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -3,4 +3,4 @@ alias eza="eza --icons=auto --header --git --long --smart-group"
 alias e=eza
 alias ee="/bin/eza"
 
-
+alias ssh="kitten ssh"
--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
+
+    # SSH agent needs to be started, lets use keychain
+    # To add a key, set -Ua SSH_KEYS_TO_AUTOLOAD keypath
+    # To remove a key, set -U --erase SSH_KEYS_TO_AUTOLOAD[index_of_key]
+    keychain --eval $SSH_KEYS_TO_AUTOLOAD >/dev/null 2>&1 | source
 end
 
-export EDITOR=hx
-export VISUAL=hx
-export PATH="$HOME/.cargo/bin:$HOME/.local/bin:$PATH"
+
+set EDITOR hx
+set VISUAL hx
+set PATH $HOME/.cargo/bin:$HOME/.local/bin:$PATH
