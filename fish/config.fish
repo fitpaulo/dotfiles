ea0329ce25a1230511d06436be42ea5116df41da
Paulo <paulotechusa@protonmail.com>
2023-12-18 08:31:10 -0600

    Daily updates

--- fish/config.fish
+++ fish/config.fish
@@ -6,8 +6,14 @@ if status is-interactive
     # To add a key, set -Ua SSH_KEYS_TO_AUTOLOAD keypath
     # To remove a key, set -U --erase SSH_KEYS_TO_AUTOLOAD[index_of_key]
     keychain --eval $SSH_KEYS_TO_AUTOLOAD >/dev/null 2>&1 | source
+
+    set EDITOR hx
+    set VISUAL hx
+
+    # load asdf
+    if test -d ~/.asdf
+        source ~/.asdf/asdf.fish
+    end
 end
 
 
-set EDITOR hx
-set VISUAL hx
